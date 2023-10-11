0b7b215412f2ea04daab2092fbf9ba5426c74f23
Xshir <Haashir150@gmail.com>
2023-10-11 15:51:25 +0800

    Add different barcode logic

--- manual/app.py
+++ manual/app.py
@@ -1,19 +1,43 @@
+import cv2
 from flask import Flask, render_template, Response
-from camera import Camera
+from pyzbar.pyzbar import decode
+from pyzbar.pyzbar import ZBarSymbol
 
-app = Flask(__name__)
+app = Flask(__name)
 
-camera = Camera()
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # Use 0 for the default camera
+    scanned_items = []
+    supported_symbols = [ZBarSymbol.CODE128, ZBarSymbol.QRCODE]
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        decoded_objects = decode(frame, symbols=supported_symbols)
+
+        if decoded_objects:
+            for object_ in decoded_objects:
+                returned_text = object_.data.decode('utf-8')
+                object_type = object_.type
+
+            if returned_text not in scanned_items:
+                scanned_items.append(returned_text)
+                print(scanned_items)
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+    cap.release()
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def generate_frames():
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
