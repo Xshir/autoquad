245fee9b869e8f5904b0b6dc38f2cbd9d96b65f2
Xshir <Haashir150@gmail.com>
2023-10-11 15:47:45 +0800

    Remove barcode logic for debugging purposes

--- manual/app.py
+++ manual/app.py
@@ -7,14 +7,13 @@ camera = Camera()
 
 @app.route('/')
 def index():
-    return render_template('index.html', scanned_items=camera.scanned_items)
+    return render_template('index.html')
 
 def generate_frames():
     while True:
         frame = camera.get_frame()
-        if frame is not None:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
@@ -22,5 +21,3 @@ def video_feed():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
--- manual/camera.py
+++ manual/camera.py
@@ -1,27 +1,10 @@
 import cv2
-from pyzbar.pyzbar import decode
-from pyzbar.pyzbar import ZBarSymbol
 
 class Camera:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)  # Use 0 for the default camera
-        self.scanned_items = []
-        self.supported_symbols = [ZBarSymbol.CODE128, ZBarSymbol.QRCODE]
 
     def get_frame(self):
         ret, frame = self.cap.read()
-        if not ret:
-            return None
-
-        decoded_objects = decode(frame, symbols=self.supported_symbols)
-
-        if decoded_objects:
-            for object_ in decoded_objects:
-                returned_text = object_.data.decode('utf-8')
-                object_type = object_.type
-
-            if returned_text not in self.scanned_items:
-                self.scanned_items.append(returned_text)
-
-        return frame
-
+        _, buffer = cv2.imencode('.jpg', frame)
+        return buffer.tobytes()
