bef4c12b30b2d1bdede51391913d43a8de556ed7
Xshir <Haashir150@gmail.com>
2023-10-11 15:29:26 +0800

    Add Camera Barcode/QR Feature

---
+++ manual/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, Response
+from camera import Camera
+
+app = Flask(__name__)
+
+camera = Camera()
+
+@app.route('/')
+def index():
+    return render_template('index.html', scanned_items=camera.scanned_items)
+
+def generate_frames():
+    while True:
+        frame = camera.get_frame()
+        if frame is not None:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+# Release the camera when the app exits
+@app.before_request
+def before_request():
+    pass
+
+@app.teardown_appcontext
+def teardown_request(exception):
+    camera.release()
---
+++ manual/camera.py
@@ -0,0 +1,11 @@
+import cv2
+
+class Camera:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)  # Use 0 for the default camera
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        _, buffer = cv2.imencode('.jpg', frame)
+        return buffer.tobytes()
+ 
