1bbae0b3b8c656d8a7ee5a41fd9d68141f153ffc
Xshir <Haashir150@gmail.com>
2023-12-20 14:15:02 +0800

    reopen ser port 2

--- lidar.py
+++ lidar.py
@@ -12,7 +12,8 @@ def read_tfluna_data():
                 bytes_serial = ser.read(9)
                 ser.reset_input_buffer()
 
-                if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59:
+                # Check if bytes_serial has enough elements before accessing them
+                if len(bytes_serial) >= 8 and bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59:
                     distance = bytes_serial[2] + bytes_serial[3] * 256
                     signal_strength = bytes_serial[4] + bytes_serial[5] * 256  # Extract signal strength
                     temperature = bytes_serial[6] + bytes_serial[7] * 256
@@ -24,8 +25,8 @@ def read_tfluna_data():
             ser.close()
             # Reopen the serial port
             ser.open()
-            time.sleep(1)
-
+            time.sleep(1)  # Add a delay to ensure the port is fully reopened
+            
 if __name__ == "__main__":
     while 1:
         print(read_tfluna_data())
