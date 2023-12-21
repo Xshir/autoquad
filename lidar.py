143761aa7f97c89c314d12f32e880575ef7b705a
Xshir <Haashir150@gmail.com>
2023-12-21 13:52:56 +0800

    potential lidar fix

--- lidar.py
+++ lidar.py
@@ -3,41 +3,23 @@ import time
 
 ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=0)
 
-import serial
-import time
-
-ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=0)
-
 def read_tfluna_data():
     while True:
-        try:
-            counter = ser.in_waiting
+        counter = ser.in_waiting # count the number of bytes of the serial port
+        if counter > 8:
+            bytes_serial = ser.read(9) # read 9 bytes
+            ser.reset_input_buffer() # reset buffer
 
-            if counter >= 9:
-                bytes_serial = ser.read(9)
-                ser.reset_input_buffer()
-
-                # Check if bytes_serial has enough elements before accessing them
-                if len(bytes_serial) >= 9 and bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59:
-                    distance = bytes_serial[2] + bytes_serial[3] * 256
-                    signal_strength = bytes_serial[4] + bytes_serial[5] * 256  # Extract signal strength
-                    temperature = bytes_serial[6] + bytes_serial[7] * 256
-                    temperature = (temperature / 8.0) - 256.0
-                    return distance / 100.0, temperature, signal_strength  # Return signal strength
-                else:
-                    print('Invalid data format:', bytes_serial)
-                    
-            time.sleep(0.1)  # Add a small delay to avoid high CPU usage in the loop
-
-        except OSError as e:
-            print(f"Error reading Lidar data: {e}")
-            # Close the serial port
-            ser.close()
-            # Reopen the serial port
-            ser.open()
-            time.sleep(1)  # Add a delay to ensure the port is fully reopened
+            if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59: # check first two bytes
+                distance = bytes_serial[2] + bytes_serial[3]*256 # distance in next two bytes
+                strength = bytes_serial[4] + bytes_serial[5]*256 # signal strength in next two bytes
+                temperature = bytes_serial[6] + bytes_serial[7]*256 # temp in next two bytes
+                temperature = (temperature/8.0) - 256.0 # temp scaling and offset
+                return distance/100.0,strength,temperature
 
 if __name__ == "__main__":
+    if ser.isOpen() == False:
+        ser.open() # open serial port if not open
     while True:
         print(read_tfluna_data())
 
