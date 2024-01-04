b30e5024e37ed7dd48a9cbf015acd45b0c95f793
Xshir <Haashir150@gmail.com>
2024-01-04 10:17:59 +0800

    fix alt hold probably?

--- app.py
+++ app.py
@@ -200,5 +200,5 @@ if __name__ == '__main__':
         text_to_speech(f"Connected to WiFi at {ssid} with ip {wlan_ip}")
         print("tts done")
         app.run(host=wlan_ip, port=5000)
-    except: pass
+    except: 
         text_to_speech(f"Failed to run app, please debug and look into logs.")
